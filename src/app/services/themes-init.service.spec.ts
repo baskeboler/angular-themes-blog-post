bfe774b1dbcdb9aa2a4adf3951a461b1f3017903
Victor Gil <baskeboler@gmail.com>
2020-02-09 06:06:48 -0300

    tests for themes init service

--- src/app/services/themes-init.service.spec.ts
+++ src/app/services/themes-init.service.spec.ts
@@ -2,18 +2,33 @@ import { TestBed } from '@angular/core/testing';
 
 import { ThemesInitService } from './themes-init.service';
 import { ThemesService } from './themes.service';
+import { Theme } from '../models/theme';
+import { Subject } from 'rxjs';
+// import 'jasmine';
 
 describe('ThemesInitService', () => {
   let service: ThemesInitService;
 
+  let themeServiceSpy = jasmine.createSpyObj<ThemesService>(['setCurrentTheme']);
+  let setCurrentThemeSpy = themeServiceSpy.setCurrentTheme.and.callFake(t => {
+    console.log('called fake set theme');
+
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ThemesService]
-    });
-    service = TestBed.inject(ThemesInitService);
+    service = new ThemesInitService(themeServiceSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should set a theme on init', async () => {
+
+    await service.init();
+    console.log('finished calling init');
+    expect(setCurrentThemeSpy.calls.any())
+      .toBeTrue();
+  });
+
 });
