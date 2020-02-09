108d705d9ab7f0d712f5ec87343330b51b33adf1
Victor Gil <baskeboler@gmail.com>
2020-02-09 04:59:14 -0300

    theme service tests

--- src/app/services/themes.service.spec.ts
+++ src/app/services/themes.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ThemesService } from './themes.service';
 import { platformBrowserTesting, BrowserTestingModule } from '@angular/platform-browser/testing';
+import { DEFAULT_THEME } from '../models/theme';
 
 describe('ThemesService', () => {
   let service: ThemesService;
@@ -14,4 +15,17 @@ describe('ThemesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should generate a random theme', () => {
+    expect(service.generateRandomTheme()).toBeDefined();
+  });
+
+  it('should notify observers when theme changes', (done: DoneFn) => {
+    service.setCurrentTheme(DEFAULT_THEME);
+    service.getCurrentTheme().subscribe(newTheme => {
+      expect(newTheme).toEqual(DEFAULT_THEME);
+      done();
+    });
+  });
+
 });
